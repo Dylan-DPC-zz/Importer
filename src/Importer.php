494c02a72e85cfbd027b7ce1bdfb6468d236ef54
dylan_DPC <dylan.dpc@gmail.com>
2017-04-18 21:08:21 +0530

    map changed to each

--- src/Importer.php
+++ src/Importer.php
@@ -6,22 +6,15 @@ use Illuminate\Support\Facades\DB;
 
 class Importer implements ImporterContract
 {
-
-
     public function import() : ImporterContract
     {
         $seeders = config('importer.seeds');
         DB::transaction(function () use ($seeders) {
-            collect($seeders)->map(function ($seeder) {
+            collect($seeders)->each(function ($seeder) {
                 $seeder = App::make($seeder);
                 $seeder->prepareData()->seed();
             });
         });
-
-
         return $this;
-
     }
-
-
 }
